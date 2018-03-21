b7ed77a43846eb667570460e1c149f1b18791a05
Duffman791 <duffortj01@sunyjcc.edu>
2018-03-20 20:24:15 -0400

    Update pay.sh

--- pay.sh
+++ pay.sh
@@ -1,1 +1,17 @@
 #/bin/bash
+
+echo "Running autopay check"
+sleep 1
+INITIAL=77777
+CURRENT=$(./staked getbalance)
+PROFIT=$((CURRENT-INITIAL-SAFE))
+SAFE=1
+
+if [ $PROFIT > $PAY ]; then
+  sudo /home/$USER/stake/src/staked sendtoaddress "$ADDR" $PROFIT
+  sleep 2
+  echo "Paid out $PROFIT"
+  echo "Kept 1 coin for safe keepings above 77777"
+else
+  echo "Profit is below $PAY"
+fi
